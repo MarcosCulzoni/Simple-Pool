ba4faa0036bcdc3957be7da262fd9bd044d8c6c5
MaracosEsteban <mculzoni@hotmail.com>
2025-04-12 16:10:16 +0200

    fin del dia

--- public/shortcodes/SPOLL_ShortcodesDashboard.php
+++ public/shortcodes/SPOLL_ShortcodesDashboard.php
@@ -17,29 +17,32 @@ class SPOLL_ShortcodesDashboard
     }
 
     public static function render_encuestas_y_exportar()
-    {
-        global $wpdb;
-        $tabla = $wpdb->prefix . "encuestas_gastronomia";
+{
+    global $wpdb;
+    $tabla = $wpdb->prefix . "encuestas_gastronomia";
 
-        $encuestas = $wpdb->get_results("SELECT id, fecha FROM $tabla ORDER BY fecha DESC");
+    $encuestas = $wpdb->get_results("SELECT id, fecha FROM $tabla ORDER BY fecha DESC");
 
-        ob_start();
+    ob_start();
 
-        echo "<div class='encuestas-listado'>";
-        echo "<ul>";
-        foreach ($encuestas as $encuesta) {
-            echo "<li>Encuesta del: " . esc_html($encuesta->fecha) . "</li>";
-        }
-        echo "</ul>";
-        echo "</div>";
-
-        $url_exportar = esc_url(admin_url('admin-post.php?action=exportar_encuestas_txt'));
-
-        echo "<form method='post' action='$url_exportar'>
-                <button type='submit'>Exportar todas las encuestas</button>
-              </form>";
-
-        return ob_get_clean();
+    echo "<div style='text-align: center;'>";
+    
+    echo "<ul style='list-style: none; padding: 0;'>";
+    foreach ($encuestas as $encuesta) {
+        echo "<li>Encuesta del: " . esc_html($encuesta->fecha) . "</li>";
     }
+    echo "</ul>";
+
+    $url_exportar = esc_url(admin_url('admin-post.php?action=exportar_encuestas_txt'));
+
+    echo "<form method='post' action='$url_exportar'>
+            <button type='submit' style='margin-top: 20px;'>Exportar todas las encuestas</button>
+          </form>";
+
+    echo "</div>";
+
+    return ob_get_clean();
+}
+
 }
 
